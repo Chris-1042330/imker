66205d5015db50a31aea12c22ee512027ea24a15
Chris-1042330 <1042330@mborijnland.nl>
2022-06-29 22:43:29 +0200

    UserController created

--- resources/views/layouts/imker.blade.php
+++ resources/views/layouts/imker.blade.php
@@ -64,16 +64,16 @@
                                     </x-slot>
 
                                     <x-slot name="content">
-                                        <!-- Authentication -->
-                                        <form method="POST" action="{{ route('logout') }}">
-                                            @csrf
+{{--                                        <!-- Authentication -->--}}
+{{--                                        <form method="POST" action="{{ route('logout') }}">--}}
+{{--                                            @csrf--}}
 
-                                            <x-dropdown-link :href="route('logout')"
-                                                             onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                                {{ __('Log Out') }}
-                                            </x-dropdown-link>
-                                        </form>
+{{--                                            <x-dropdown-link :href="route('logout')"--}}
+{{--                                                             onclick="event.preventDefault();--}}
+{{--                                                this.closest('form').submit();">--}}
+{{--                                                {{ __('Log Out') }}--}}
+{{--                                            </x-dropdown-link>--}}
+{{--                                        </form>--}}
                                     </x-slot>
                                 </x-dropdown>
                             </div>
