34c51cd958aebfe830d31853d17c5c90b7f93a91
Chris-1042330 <1042330@mborijnland.nl>
2022-06-30 13:29:18 +0200

    Update imker.blade.php

--- resources/views/layouts/imker.blade.php
+++ resources/views/layouts/imker.blade.php
@@ -52,7 +52,7 @@
                                     <x-nav-link :href="__('cursussen')" :active="request()->routeIs('cursussen')">
                                         {{ __('Cursussen') }}
                                     </x-nav-link>
-                                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                    <div class="hidden sm:flex sm:items-center sm:ml-6" style="margin-left: 0">
                                         <x-dropdown align="left" width="48">
                                             <x-slot name="trigger">
                                                 <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -64,15 +64,15 @@
                                                 </button>
                                             </x-slot>
                                             <x-slot name="content">
-                                                <x-nav-link :href="route('adm/users')" :active="request()->routeIs('Basiscursus')">
+                                                <x-dropdown-link :href="route('adm/users')" :active="request()->routeIs('Basiscursus')">
                                                     {{ __('Basiscursus') }}
-                                                </x-nav-link>
-                                                <x-nav-link :href="route('adm/users')" :active="request()->routeIs('Bijscholing')">
+                                                </x-dropdown-link>
+                                                <x-dropdown-link :href="route('adm/users')" :active="request()->routeIs('Bijscholing')">
                                                     {{ __('Bijscholing') }}
-                                                </x-nav-link>
-                                                <x-nav-link :href="route('adm/users')" :active="request()->routeIs('Cursusblog')">
+                                                </x-dropdown-link>
+                                                <x-dropdown-link :href="route('adm/users')" :active="request()->routeIs('Cursusblog')">
                                                     {{ __('Cursusblog') }}
-                                                </x-nav-link>
+                                                </x-dropdown-link>
                                             </x-slot>
                                         </x-dropdown>
                                     </div>
