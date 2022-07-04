aa99bdb76b7c9825d0db3a538f50034221c3e86c
Chris-1042330 <1042330@mborijnland.nl>
2022-07-04 12:31:44 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @section('title', 'Home')
 @include('layouts.imker')
-<img src="img/stuifmeelraam-scaled.jpg">
-    <section>
-        <h1>JBYUDSHFYUSDHFUIHDSYUFHSU</h1>
-    </section>
+<section class="">
+    <img src="img/stuifmeelraam-scaled.jpg" class="mx-auto">
+    <h1 class="text-center text-2xl">Welkom bij de imkervereniging!</h1>
+</section>
 @include('layouts.footer')
